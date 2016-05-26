3ad8123d43e94c37aa157dc1d3f7e8c791ea83a8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-05-27 00:46:18 +0300

    esp8266/scripts/inisetup: Start WebREPL daemon on boot.

--- esp8266/scripts/inisetup.py
+++ esp8266/scripts/inisetup.py
@@ -41,8 +41,8 @@ def setup():
         f.write("""\
 # This file is executed on every boot (including wake-boot from deepsleep)
 import gc
-#import webrepl
-#webrepl.start()
+import webrepl
+webrepl.start()
 gc.collect()
 """)
     return vfs
